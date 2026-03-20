70d71d18af7e3730156265f610de517849bd1945
eriyknas <anastasia9cheshuina@yandex.ru>
2026-03-20 09:46:46 +0300

    Step 10: Top App Bar added

--- app/src/main/java/com/shmal/student_material_design/MainActivity.kt
+++ app/src/main/java/com/shmal/student_material_design/MainActivity.kt
@@ -16,11 +16,15 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
@@ -31,6 +35,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.shmal.student_material_design.data.Student
 import com.shmal.student_material_design.data.students
 import com.shmal.student_material_design.ui.theme.Student_Material_DesignTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -102,12 +107,19 @@ fun StudentItem(
 
 @Composable
 fun StudentApp() {
-    LazyColumn {
-        items(students){
-            StudentItem(
-                student = it,
-                modifier = Modifier
-                    .padding(dimensionResource(R.dimen.padding_small)))
+    Scaffold(
+        topBar = {
+            StudentTopAppBar()
+        }
+    ) { it ->
+        LazyColumn (contentPadding = it) {
+            items(students) {
+                StudentItem(
+                    student = it,
+                    modifier = Modifier
+                        .padding(dimensionResource(R.dimen.padding_small))
+                )
+            }
         }
     }
 }
@@ -126,4 +138,32 @@ fun StudentDarkThemePreview(){
     Student_Material_DesignTheme(darkTheme = true) {
         StudentApp()
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun StudentTopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    modifier = Modifier
+                        .size(dimensionResource(R.dimen.image_size))
+                        .padding(dimensionResource(R.dimen.padding_small)),
+                    painter = painterResource(R.drawable.ic_student_logo),
+                    contentDescription = null
+                )
+                Text(
+                    text = stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.displayLarge
+                )
+            }
+        },
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        ),
+        modifier = modifier
+    )
 }
